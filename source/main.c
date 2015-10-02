a8417f542827e07b84163c76626cd24ee07d7026
d0k3 <tore.anon@gmail.com>
2015-10-02 04:04:11 +0200

    Changes to make Brahma II run payloads directly

--- source/main.c
+++ source/main.c
@@ -1,79 +1,18 @@
 #include <3ds.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <malloc.h>
+#include "payload_bin.h"
 #include "brahma.h"
-#include "hid.h"
-#include "menus.h"
-#include "sochlp.h"
 
-void interact_with_user (void) {
-	s32 menuidx = 0;
-	
-	while (aptMainLoop()) {
-		gspWaitForVBlank();
-
-		menuidx = print_main_menu(menuidx, &g_main_menu);		
-
-		u32 kDown = wait_key(); 
-		
-		if (kDown & KEY_B) {
-			break;
-		}
-		else if (kDown & KEY_A) {
-			consoleClear();
-			printf("\n");
-			if (menu_execute_function(menuidx, &g_main_menu, 0))
-				wait_any_key();
-		}
-		else if (kDown & KEY_UP) {
-			menuidx--;
-		}
-		else if (kDown & KEY_DOWN) {
-			menuidx++;
-		}
-		gfxFlushBuffers();
-		gfxSwapBuffers(); 
-	}
-
-	return;
-}
-
-s32 quick_boot_firm (s32 load_from_disk) {
-	if (load_from_disk)
-		load_arm9_payload("/arm9payload.bin");
-	firm_reboot();	
-}
 
 s32 main (void) {
 	// Initialize services
 	gfxInitDefault();
 	gfxSwapBuffers(); 
 	
-	consoleInit(GFX_BOTTOM, NULL);
 	if (brahma_init()) {
-		hidScanInput();
-		u32 kHeld = hidKeysHeld();
-	 
-		if (kHeld & KEY_LEFT) {
-			/* load default payload from dosk and run exploit */
-			quick_boot_firm(1);
-			printf("[!] Quickload failed\n");
-			wait_any_key();
-		} else if (kHeld & KEY_RIGHT) {
-			/* reboot only */
-			quick_boot_firm(0);
-		}
-	
-		soc_init();	
-		interact_with_user();
-		soc_exit();
+		// load_arm9_payload("payload.bin");
+		load_arm9_payload_from_mem(payload_bin, payload_bin_size);
+		firm_reboot();
 		brahma_exit();
-
-	} else {
-		printf("* BRAHMA *\n\n[!]Not enough memory\n");
-		wait_any_key();
 	}
 
 	gfxExit();
