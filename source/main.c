499b51e428b59390abc9243cd1435230e3606874
d0k3 <tore.anon@gmail.com>
2015-10-30 13:01:17 +0100

    Allow start parameters / auto loading of payloads

--- source/main.c
+++ source/main.c
@@ -46,31 +46,49 @@ s32 quick_boot_firm (s32 load_from_disk) {
 	firm_reboot();
 }
 
-s32 main (void) {
+s32 main (int argc, char **argv) {
 	// Initialize services
 	gfxInitDefault();
 	gfxSwapBuffers(); 
 	
 	consoleInit(GFX_BOTTOM, NULL);
 	if (brahma_init()) {
-		hidScanInput();
-		u32 kHeld = hidKeysHeld();
-	 
-		if (kHeld & KEY_LEFT) {
-			/* load default payload from dosk and run exploit */
-			quick_boot_firm(1);
-			printf("[!] Quickload failed\n");
+		if (argc > 1) {
+			char payload[128];
+			u32 offset;
+			u32 psize;
+			
+			u32 res = sscanf(argv[1], "%128s %X %X", payload, &offset, &psize);
+			if (res < 3) psize = 0;
+			if (res < 2) offset = 0;
+			if (res < 1) {
+				printf("[!] Invalid argument: %s\n", argv[1]);
+			} else {
+				printf("[+] payload: %s@%X\n[+] maximum size: %X\n", payload, offset, psize);
+				if (load_arm9_payload(payload, offset, psize))
+					firm_reboot();
+				printf("[!] Loading failed\n");
+			}
 			wait_any_key();
-		} else if (kHeld & KEY_RIGHT) {
-			/* reboot only */
-			quick_boot_firm(0);
-		}
-	
-		soc_init();	
-		interact_with_user();
-		soc_exit();
-		brahma_exit();
+		} else {
+			hidScanInput();
+			u32 kHeld = hidKeysHeld();
+		 
+			if (kHeld & KEY_LEFT) {
+				/* load default payload from dosk and run exploit */
+				quick_boot_firm(1);
+				printf("[!] Quickload failed\n");
+				wait_any_key();
+			} else if (kHeld & KEY_RIGHT) {
+				/* reboot only */
+				quick_boot_firm(0);
+			}
 
+			soc_init();	
+			interact_with_user();
+			soc_exit();
+			brahma_exit();
+		}
 	} else {
 		printf("* BRAHMA *\n\n[!]Not enough memory\n");
 		wait_any_key();
